741a10bc7edb48ad9448a3621f2eaea628f458b9
tkmageztik <jujex_90@hotmail.com>
2018-01-28 23:56:56 -0500

    varios.

--- NS.BFX_amarin/NS.BFX_amarin/MainPage.xaml.cs
+++ NS.BFX_amarin/NS.BFX_amarin/MainPage.xaml.cs
@@ -12,46 +12,51 @@ namespace NS.BFX_amarin
         public MainPage()
         {
             InitializeComponent();
-            GetMainPage();
+            //GetMainPage();
 		}
 
-        public static Page GetMainPage()
+        protected void BtnIngresar_OnClicked()
         {
-            
-            
-            var myLabel = new Label()
-            {
-                Text = "Hello World",
-                FontSize = 20,
-                TextColor = Color.White,
-                HorizontalTextAlignment = TextAlignment.Center,
-                VerticalTextAlignment = TextAlignment.Center
-            };
-
-            var myImage = new Image()
-            {
-                Source = FileImageSource.FromUri(
-                    new Uri("http://xamarin.com/content/images/pages/index/hero-slide.jpg"))
-            };
-
-            RelativeLayout layout = new RelativeLayout();
-
-            layout.Children.Add(myImage,
-                Constraint.Constant(0),
-                Constraint.Constant(0), 
-                Constraint.RelativeToParent((parent) => { return parent.Width; }),
-                Constraint.RelativeToParent((parent) => { return parent.Height; }));
-
-            layout.Children.Add(myLabel,
-                Constraint.Constant(0),
-                Constraint.Constant(0),
-                Constraint.RelativeToParent((parent) => { return parent.Width; }),
-                Constraint.RelativeToParent((parent) => { return parent.Height; }));
-
-            return new ContentPage
-            {
-                Content = layout
-            };
+            DisplayAlert("Mensaje", "Hola nena JM", "ok");
         }
+
+        //public static Page GetMainPage()
+        //{
+
+
+        //    var myLabel = new Label()
+        //    {
+        //        Text = "Hello World",
+        //        FontSize = 20,
+        //        TextColor = Color.White,
+        //        HorizontalTextAlignment = TextAlignment.Center,
+        //        VerticalTextAlignment = TextAlignment.Center
+        //    };
+
+        //    var myImage = new Image()
+        //    {
+        //        Source = FileImageSource.FromUri(
+        //            new Uri("http://xamarin.com/content/images/pages/index/hero-slide.jpg"))
+        //    };
+
+        //    RelativeLayout layout = new RelativeLayout();
+
+        //    layout.Children.Add(myImage,
+        //        Constraint.Constant(0),
+        //        Constraint.Constant(0), 
+        //        Constraint.RelativeToParent((parent) => { return parent.Width; }),
+        //        Constraint.RelativeToParent((parent) => { return parent.Height; }));
+
+        //    layout.Children.Add(myLabel,
+        //        Constraint.Constant(0),
+        //        Constraint.Constant(0),
+        //        Constraint.RelativeToParent((parent) => { return parent.Width; }),
+        //        Constraint.RelativeToParent((parent) => { return parent.Height; }));
+
+        //    return new ContentPage
+        //    {
+        //        Content = layout
+        //    };
+        //}
     }
 }
