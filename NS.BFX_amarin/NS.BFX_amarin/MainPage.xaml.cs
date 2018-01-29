7b5f899b68e976b9da608ec587e2263121722b1c
tkmageztik <jujex_90@hotmail.com>
2018-01-29 00:49:31 -0500

    se finaliza primera etapa configuración de release, el linking eliminaba mi display alert... .

--- NS.BFX_amarin/NS.BFX_amarin/MainPage.xaml.cs
+++ NS.BFX_amarin/NS.BFX_amarin/MainPage.xaml.cs
@@ -15,9 +15,19 @@ namespace NS.BFX_amarin
             //GetMainPage();
 		}
 
-        protected void BtnIngresar_OnClicked()
+        private void BtnIngresar_OnClicked(object sender,EventArgs args)
         {
             DisplayAlert("Mensaje", "Hola nena JM", "ok");
+        }
+
+        public static bool Luhn(string digits)
+        {
+            return digits.All(char.IsDigit) && digits.Reverse()
+                .Select(c => c - 48)
+                .Select((thisNum, i) => i % 2 == 0
+                    ? thisNum
+                    : ((thisNum *= 2) > 9 ? thisNum - 9 : thisNum)
+                ).Sum() % 10 == 0;
         }
 
         //public static Page GetMainPage()
